866bc2a4b62af101e619f87c14343da834fdd3a4
feath <feathong985@gmail.com>
2026-04-08 22:48:16 +0800

    Update: Update Dependencies

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,11 @@ dotenv.config({
 const config = {
   PORT: process.env.PORT || 8000, //port
   NODE_ENV: process.env.NODE_ENV, //node environment
-  WHITELIST_ORIGIN: process.env.ALLOWED_DOMAIN?.split(',') || '', //allowed domain for CORS
+  //allowed domain for CORS
+  WHITELIST_ORIGIN: (process.env.ALLOWED_DOMAIN || '')
+    .split(',')
+    .map((origin) => origin.trim())
+    .filter(Boolean),
   MONGO_URI: process.env.MONGO_URI, //mongodb uri
   LOG_LEVEL: process.env.LOG_LEVEL || 'info', //for setting winston log level
   LOG_TYPE: process.env.LOG_TYPE || 'json', //for setting winston log type (json || custom)
